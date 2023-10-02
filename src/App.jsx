074a9ab24c0e4b383a509083d0d8eba0bf0f21e8
Jose Vasquez <josevasquez8365@gmail.com>
2023-10-02 14:39:17 -0400

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,42 +1,42 @@
-import React, { useEffect, useRef } from 'react'
+import React, { useEffect, useRef } from "react";
 
-import { Cuadrito } from './components/Cuadrito'
-import "./styles.css"
-import { randomColors } from './utilities/randomColor'
-
-
+import { Cuadrito } from "./components/Cuadrito";
+import "./styles.css";
+import { randomColors } from "./utilities/randomColor";
 
 export const App = () => {
-    const containerRef = useRef()
+  const containerRef = useRef();
 
+  useEffect(() => {
+    const containerElement = containerRef.current;
 
-    useEffect(() => {
-        const containerElement = containerRef.current
-
-        const handleMouseMove = (event) => {
+    const handleMouseMove = (event) => {
+      if (event.target.classList.contains("cuadrito")) {
+        event.target.addEventListener("mouseleave", () => {
+            setTimeout(()=> {
+                event.target.style.background = "#1d1d1d";
+            },500)
           
-           
-            if (event.target.classList.contains('cuadrito')) {
-                event.target.style.background = `${randomColors[Math.floor(Math.random()*randomColors.length)]}`
-                setTimeout(()=> {
-                    event.target.style.background = "#1d1d1d"
-                },500)
-                
-            }
-        }
+        });
+        event.target.style.background = `${
+          randomColors[Math.floor(Math.random() * randomColors.length)]
+        }`;
+      }
+    };
 
-        containerElement.addEventListener("mouseover", handleMouseMove)
+    containerElement.addEventListener("mouseover", handleMouseMove);
+    containerElement;
 
-        return () => {
-            containerElement.removeEventListener("mouseover", handleMouseMove)
-        }
-    }, [])
+    return () => {
+      containerElement.removeEventListener("mouseover", handleMouseMove);
+    };
+  }, []);
 
   return (
     <div className="container" ref={containerRef}>
-         {Array.from({ length: 460 }).map((_, index) => (
-        <Cuadrito key={index}/>
+      {Array.from({ length: 460 }).map((_, index) => (
+        <Cuadrito key={index} />
       ))}
     </div>
-  )
-}
+  );
+};
--- src/components/Cuadrito.jsx
+++ src/components/Cuadrito.jsx
@@ -1,8 +1,10 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import "./Cuadrito.css"
 
 export const Cuadrito = () => {
+
+
   return (
-    <div className='cuadrito'></div>
+    <div className='cuadrito' ></div>
   )
 }
